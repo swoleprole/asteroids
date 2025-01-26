d5b8e7c1073e8a14de373def8586d0c2a6a53fc2
swoleprole <notmyrealemail81393@gmail.com>
2025-01-26 21:41:07 +0000

    Add sprites, player, and rendering for player

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import pygame
 
 from constants import *
 
+from player import Player
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
