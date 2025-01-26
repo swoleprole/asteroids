b8aebedc059db63e0316d15e4d9545dce503d1df
swoleprole <notmyrealemail81393@gmail.com>
2025-01-26 23:08:38 +0000

    add player rotation functionality

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        player.update(dt)    
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
